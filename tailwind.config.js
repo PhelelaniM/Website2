de244861998e73125a3222d7988867fb9c887973
PhelelaniM <Phelelanim8@gmail.com>
2025-01-08 19:32:55 +0200

    CSS issues fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'teal': {
+          500: '#14b8a6',
+        },
+      },
+    },
   },
   plugins: [],
 }
